be0ce7ee1f7475506c10b45bef24c98e80d7c188
Benjamin Crout <benjamin.crout@gmail.com>
2014-10-15 09:56:33 -0600

    Created and finished the lrthw_ex2.rb file, it can be run with the correct output from the command line.

---
+++ LRTHW/lrthw_ex2.rb
@@ -0,0 +1,11 @@
+!#/usr/bin/env ruby
+
+# A comment, this will be viewed as plain test inside of the program.
+# Anything after the # (hash) is ignored by Ruby.
+
+puts "I could have code like this." # and the comment after will be ignored.
+
+# You may also use comments to 'disable' certain pieces of code.
+# puts "This wont run."
+
+puts "This will run."
