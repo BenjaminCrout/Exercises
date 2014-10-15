73d2516ed49f068a55b317ea9da02494cab757ab
Benjamin Crout <benjamin.crout@gmail.com>
2014-10-14 22:50:11 -0600

    Updated the LRTHW directory to enable the use of rubocop and rspec.

--- LRTHW/lrthw_ex1.rb
+++ LRTHW/lrthw_ex1.rb
@@ -1,4 +1,4 @@
-!#/usr/bin/env ruby
+! # /usr/bin/env ruby
 
 # puts "Hello World!"
 # puts "Hello Again"
@@ -8,4 +8,5 @@
 # puts "I'd much rather you 'not'."
 # puts 'I "said" do not touch this.'
 puts 'This line is extra, and was not here before.'
-puts 'An octothorpe is also called a "hash", and in Ruby code it will comment out the line that follows it.'
+puts 'An octothorpe is also called a "hash", and in Ruby code it will" +
+" comment out the line that follows it.'
