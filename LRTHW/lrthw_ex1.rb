3340c2f3e9c2fafe8e54497942b18f8e68cda388
Benjamin Crout <benjamin.crout@gmail.com>
2014-10-14 22:12:28 -0600

    lrthw_ex1.rb is now complete, including the extras.

--- LRTHW/lrthw_ex1.rb
+++ LRTHW/lrthw_ex1.rb
@@ -1,9 +1,11 @@
 !#/usr/bin/env ruby
 
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+puts 'This line is extra, and was not here before.'
+puts 'An octothorpe is also called a "hash", and in Ruby code it will comment out the line that follows it.'
