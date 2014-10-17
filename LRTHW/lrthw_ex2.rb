46e286d3f63ad97c2bdc7be4824acc354ae164b3
Benjamin Crout <benjamin.crout@gmail.com>
2014-10-17 17:24:00 -0600

    Includes the updated lrthw_ex2.rb file.

--- LRTHW/lrthw_ex2.rb
+++ LRTHW/lrthw_ex2.rb
@@ -3,9 +3,9 @@
 # A comment, this will be viewed as plain test inside of the program.
 # Anything after the # (hash) is ignored by Ruby.
 
-puts "I could have code like this." # and the comment after will be ignored.
+puts 'I could have code like this.' # and the comment after will be ignored.
 
 # You may also use comments to 'disable' certain pieces of code.
 # puts "This wont run."
 
-puts "This will run."
+puts 'This will run.'
